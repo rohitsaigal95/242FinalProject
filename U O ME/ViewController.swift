9e9a50dc05a505760133fdf98f47e80976182d41
Collin Walther <cwalthe2@illinois.edu>
2016-11-01 18:33:19 -0500

    first commit
    set up barely anything

---
+++ U O ME/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  U O ME
+//
+//  Created by Collin Walther on 11/1/16.
+//  Copyright © 2016 Collin Walther, Rohit Saigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
